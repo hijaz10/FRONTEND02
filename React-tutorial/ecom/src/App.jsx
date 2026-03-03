9e5c00e8aae6f3692efcc299bd4fb5c42bef89e5
HIJAZ <muhdhijaz10@gmail.com>
2026-03-03 12:55:33 +0100

    Functional Checkout page

--- React-tutorial/ecom/src/App.jsx
+++ React-tutorial/ecom/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router'
 import './App.css'
 import HomePage from './components/HomePage'
+import CheckoutPage from './components/CheckoutPage'
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
     <>
     <Routes>
       <Route path='/' element={<HomePage/>}></Route>
-      <Route path='/checkout' element={KK}></Route>
+      <Route path='/checkout' element={<CheckoutPage/>}></Route>
       <Route path='/Logout' element={ <h1>LOGOUT PAGE</h1>}></Route>
       <Route path='/Login' element={ <h1>LOGIN PAGE</h1>}></Route>
     </Routes>
