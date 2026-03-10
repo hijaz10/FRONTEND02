c3f547ee09bfe183f04a17b458235b5be044f385
HIJAZ <muhdhijaz10@gmail.com>
2026-03-10 12:18:20 +0100

    Ltest

--- React-tutorial/ecom/src/App.jsx
+++ React-tutorial/ecom/src/App.jsx
@@ -1,21 +1,20 @@
-import { Routes, Route } from 'react-router'
-import './App.css'
-import HomePage from './components/HomePage'
-import CheckoutPage from './components/CheckoutPage'
+import { Routes, Route } from "react-router";
+import "./App.css";
+import HomePage from "./components/HomePage";
+import CheckoutPage from "./components/CheckoutPage";
 
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route path='/' element={<HomePage/>}></Route>
-      <Route path='/checkout' element={<CheckoutPage/>}></Route>
-      <Route path='/Logout' element={ <h1>LOGOUT PAGE</h1>}></Route>
-      <Route path='/Login' element={ <h1>LOGIN PAGE</h1>}></Route>
-    </Routes>
-    
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/checkout" element={<CheckoutPage />}></Route>
+        <Route path="/Logout" element={<h1>LOGOUT PAGE</h1>}></Route>
+        <Route path="/Login" element={<h1>LOGIN PAGE</h1>}></Route>
+        <Route path="/Login" element={<h1>LOGIN PAGE</h1>}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
